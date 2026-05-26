ca9d619b7bb7aab7a541bf518330cf454d116956
marcoufesiee <florian.marcou@edu.esiee.fr>
2026-05-26 12:25:23 +0200

    Camera and power had been add

---
+++ src/camera.py
@@ -0,0 +1,8 @@
+from picamera2 import Picamera2 #pour piloter la caméra
+import os #pour créer un fichier de sauvegarde
+import datetime #pour horodater le fichier
+from config import CAMERA_WIDTH, CAMERA_HEIGHT, SAVE_DIR
+
+SD_ADRESS = SAVE_DIR
+
+
