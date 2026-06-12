610b4b87bc299cf6c4539889f92981a95f97aa27
Zoé de Filippis <zoe.defilippis@edu.esiee.fr>
2026-06-12 10:43:30 +0200

    Test IA

--- src/emotion_detector.py
+++ src/emotion_detector.py
@@ -25,18 +25,6 @@ class EmotionResNet(nn.Module):
     def forward(self, x):
         return self.backbone(x)
 
-class EmotionResNet(nn.Module):
-    def __init__(self, num_classes=5):
-        super().__init__()
-        self.backbone = models.resnet18(weights=None) #couches neuronales qui reprend resnet
-        self.backbone.fc = nn.Sequential(
-            nn.Dropout(0.5),
-            nn.Linear(512, 256),
-            nn.ReLU(),
-            nn.Dropout(0.3),
-            nn.Linear(256, num_classes)
-        )
-
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = EmotionResNet(num_classes=5).to(device)
 model.load_state_dict(torch.load(MODEL_
